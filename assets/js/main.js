a0dc615fc1b48084ed8162f07207ca6e2e797263
domieee <99103235+domieee@users.noreply.github.com>
2023-01-06 11:55:02 +0100

    added comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 function intro() {
     let a = 1 + 3;
+        // Deklariert eine mathematische Operation als Variable
     console.log("Hello world");
+        // Gibt den String "Hello world" in der Konsole aus
     console.log("1+3 = " + a);
+        // Gibt den String "1+3 = " + die Summe der Variable die wir zuvor definiert haben
 }
 
 intro();
+    // Ruft unseren Funktionsblock auf
